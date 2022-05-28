d3614fd92540ab875a84057a90f0615e285ab426
Shimon Mondol <shimonuap072@gmail.com>
2022-05-28 22:16:37 +0600

    readme add

---
+++ README.md
@@ -0,0 +1,20 @@
+# My Live Website Link : 
+
+
+
+# My Project name is 
+
+Computer-Solution
+
+# My Work
+
+* Visit & Buy computer parts of Computer-Solution
+
+* Free Registration and Login option
+
+* When you Login the you can see your Dashboard
+
+* Some good review of my Website
+
+* You can see my Portfolio
+
--- index.js
+++ index.js
@@ -17,12 +17,19 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('computer_solution').collection('services');
+        const orderCollection = client.db('computer_solution').collection('orders');
 
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        });
+
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
         })
     }
     finally {
